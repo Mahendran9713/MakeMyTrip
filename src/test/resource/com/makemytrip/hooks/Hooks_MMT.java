4f334b4c9e1bc14fbf3339c2adf49690457e7897
Mahendran9713 <mahendranscom97@gmail.com>
2026-08-18 18:20:19 +0530

    Testing purpose

--- src/test/resource/com/makemytrip/hooks/Hooks_MMT.java
+++ src/test/resource/com/makemytrip/hooks/Hooks_MMT.java
@@ -21,3 +21,23 @@ public class Hooks_MMT {
 	}
 
 }
+
+//updated by Mahendran
+
+public class Hooks_MMT {
+	
+	@Before
+	private void beforeHooks(Scenario scenario) {
+		System.out.println(scenario.getName());
+	}
+	
+	@After
+	private void afterHooks(Scenario scenario) {
+		String status = scenario.getStatus();
+		System.out.println(scenario.getStatus());
+		if(status=="failed") {
+			
+		}
+	}
+
+}
